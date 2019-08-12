1256e346477cf7965bc213593620a32ea84bdf9a
meenuaviox <meenuaviox@gmail.com>
2019-08-12 19:08:25 +0530

    user edit view

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 from django.urls import path
+from dashboard import views
 from .views import *
 
 urlpatterns = [
 	path('',LoginView.as_view(),name='login'),
-	path('dashboard',IndexView.as_view(), name='index'),
+
+	# ===============  Users ====== #
+
+	path('dashboard',UserView.as_view(), name='index'),
+	path('active-delete',UserActive.as_view(),name='active_delete'),
+	path('user-edit/<int:userid>',UserEdit.as_view(),name='user_edit'),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -6,14 +6,33 @@ from django.contrib.auth import authenticate,login
 from django.contrib.auth.models import User
 from api.models import *
 import json
+from django.contrib import messages
+from django.http import JsonResponse
 
+class LoginView(TemplateView):
+	template_name='login.html'
 
-class IndexView(View):
-	template_name='index.html'
+	def get(self,request,*args, **kwargs):
+		return render(request,self.template_name,locals())
+
+	def post(self,request):
+		username=request.POST.get('user')
+		password=request.POST.get('passwd')
+		user=authenticate(username=username, password=password)
+		if user:
+			login(request,user)
+			return HttpResponseRedirect('/dashboard')
+
+		else:
+			return render(request,self.template_name,locals())
+
+class UserView(View):
+	template_name='user.html'
 
 	def get(self, request, *args, **kwargs):
-		user=User.objects.all()
-		user_list = json.dumps([u.username for u in user])
+		users = UserModel.objects.all()
+		user_list = json.dumps([u.user.username for u in users])
+
 		return render(request,self.template_name,locals())
 
 	def post(self,request):
@@ -40,21 +59,53 @@ class IndexView(View):
 		user_obj.save()
 		return HttpResponseRedirect('/dashboard')
 
-class LoginView(TemplateView):
-	template_name='login.html'
 
-	def get(self,request,*args, **kwargs):
-		return render(request,self.template_name,locals())
+class UserActive(View):
 
-	def post(self,request):
-		username=request.POST.get('user')
-		password=request.POST.get('passwd')
-		user=authenticate(username=username, password=password)
-		print(user)
-		if user:
-			login(request,user)
-			return HttpResponseRedirect('/dashboard')
+	def get(self, request):
+		user_id = request.GET.get("user_id")
+		User.objects.get(id=user_id).delete()
+		messages.success(request,"user is successfully deleted")
+		return HttpResponseRedirect('/dashboard')
 
+	def post(self,request,*args, **kwargs):
+		user_id = request.POST.get("id")
+		user= User.objects.get(id=user_id)
+		if user.is_active == True:
+			user.is_active = False
 		else:
-			return render(request,self.template_name,locals())
+			user.is_active = True
+		user.save()
+		return HttpResponseRedirect("/dashboard")
+
+class UserEdit(View):
+	template_name='user_edit.html'
+	def get(self,request,userid):
+		user_id=userid
+		user_data=User.objects.get(id=user_id)
+		
+		return render(request, self.template_name, locals())
+
+	def post(self,request,userid):
+		user_id=userid
+		first_name=request.POST.get('fname')
+		last_name=request.POST.get('lname')
+		email_id=request.POST.get('email')
+		phone=request.POST.get('pnum')
+		image=request.FILES.get('img')
+		dob=request.POST.get('dob')
+		gender=request.POST.get('gender')
+		
+		user_obj=User.objects.get(id=user_id)
+		user_obj.first_name=first_name
+		user_obj.last_name=last_name
+		user_obj.email=email_id
+		user_obj.usermodel.phone=phone
+		user_obj.usermodel.image=image
+		user_obj.usermodel.dob=dob
+		user_obj.usermodel.gender=gender
+		user_obj.save()
+		return HttpResponseRedirect('/dashboard')
+
+
 		
