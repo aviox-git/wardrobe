fd7d98cb99ab5e09c719a55c50b21caec554bde1
meenuaviox <meenuaviox@gmail.com>
2019-08-13 16:13:31 +0530

    edit template

--- dashboard/views.py
+++ dashboard/views.py
@@ -8,6 +8,7 @@ from api.models import *
 import json
 from django.contrib import messages
 from django.http import JsonResponse
+import datetime
 
 class LoginView(TemplateView):
 	template_name='login.html'
@@ -32,31 +33,41 @@ class UserView(View):
 	def get(self, request, *args, **kwargs):
 		users = UserModel.objects.all()
 		user_list = json.dumps([u.user.username for u in users])
-
 		return render(request,self.template_name,locals())
 
 	def post(self,request):
-		username=request.POST.get('username')
-		password=request.POST.get('createpass')
-		first_name=request.POST.get('fname')
-		last_name=request.POST.get('lname')
-		email_id=request.POST.get('emailid')
-		user=User.objects.create(
-			username=username,
-			email=email_id,
-			)
-		user.set_password(password)
-		user.first_name=first_name
-		user.last_name=last_name
-		user.save()
-		user_obj=UserModel(
-			user=user,
-			phone=request.POST.get('pnumber'),
-			image=request.FILES.get('img'),
-			dob=request.POST.get('dob'),
+		try:
+			username=request.POST.get('username')
+			password=request.POST.get('createpass')
+			first_name=request.POST.get('fname')
+			last_name=request.POST.get('lname')
+			email_id=request.POST.get('emailid')
+			dob=request.POST.get('dob')
+			phone=request.POST.get('pnumber')
+			image=request.FILES.get('img')
 			gender=request.POST.get('gender')
-			)
-		user_obj.save()
+
+			dob=datetime.datetime.strptime(dob,"%d-%m-%Y")
+			user=User.objects.create(
+				username=username,
+				email=email_id,
+				)
+			user.set_password(password)
+			user.first_name=first_name
+			user.last_name=last_name
+			user.save()
+			print(user, phone,image,gender)
+			user_obj=UserModel(
+				user=user,
+				image=image,
+				dob=dob,
+				gender=gender
+				)
+			if phone:
+				user_obj.phone = phone
+			user_obj.save()
+		except Exception as e:
+			raise e
 		return HttpResponseRedirect('/dashboard')
 
 
@@ -64,6 +75,8 @@ class UserActive(View):
 
 	def get(self, request):
 		user_id = request.GET.get("user_id")
+		print('user',user_id)
+		print('user objects',User.objects.get(id=user_id))
 		User.objects.get(id=user_id).delete()
 		messages.success(request,"user is successfully deleted")
 		return HttpResponseRedirect('/dashboard')
@@ -83,7 +96,8 @@ class UserEdit(View):
 	def get(self,request,userid):
 		user_id=userid
 		user_data=User.objects.get(id=user_id)
-		
+		date=user_data.usermodel.dob.strftime("%d-%m-%Y")
+		print(date)
 		return render(request, self.template_name, locals())
 
 	def post(self,request,userid):
@@ -95,16 +109,21 @@ class UserEdit(View):
 		image=request.FILES.get('img')
 		dob=request.POST.get('dob')
 		gender=request.POST.get('gender')
+		print(dob)
+		date=datetime.datetime.strptime(dob,"%d-%m-%Y")
 		
 		user_obj=User.objects.get(id=user_id)
 		user_obj.first_name=first_name
 		user_obj.last_name=last_name
 		user_obj.email=email_id
-		user_obj.usermodel.phone=phone
-		user_obj.usermodel.image=image
-		user_obj.usermodel.dob=dob
+		if phone:
+			user_obj.usermodel.phone=phone
+		if image:
+			user_obj.usermodel.image=image
+		user_obj.usermodel.dob=date
 		user_obj.usermodel.gender=gender
 		user_obj.save()
+		user_obj.usermodel.save()
 		return HttpResponseRedirect('/dashboard')
 
 
